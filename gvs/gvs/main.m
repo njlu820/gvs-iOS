ed556b8197bf2ba468d50712a6cf6abb9a596d0d
Comus <comus.leong@gmail.com>
2014-05-15 08:35:36 +0800

    create Xcode empty project

---
+++ gvs/gvs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gvs
+//
+//  Created by Comus on 15/5/14.
+//  Copyright (c) 2014 gv-technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ gvs/gvsTests/gvsTests.m
@@ -0,0 +1,34 @@
+//
+//  gvsTests.m
+//  gvsTests
+//
+//  Created by Comus on 15/5/14.
+//  Copyright (c) 2014 gv-technology. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gvsTests : XCTestCase
+
+@end
+
+@implementation gvsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
